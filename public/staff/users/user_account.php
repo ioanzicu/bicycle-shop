bec273ab6ccf9c4d43b31c81d33b5aa2ff607168
Ioan Zicu <ioan.zicu94@gmail.com>
2019-05-26 16:47:17 +0200

    Style Buttons in User Account

--- public/staff/users/user_account.php
+++ public/staff/users/user_account.php
@@ -13,16 +13,14 @@ if(!$user = User::find_by_id($id)){
 
 <div id="content">
 	<div id="main-menu">
-	    <h2>Hello <?php echo $cookie->get_username(); ?>!!!</h2>
 	    	<?php $page_title = 'Show User: ' . h($user->full_name()); ?>
 
 			<div id="content">
 
-			  	<div class="admin show">
-
-				<h1>User: <?php echo h($user->full_name()); ?></h1>
-
-				<div class="attributes">
+			  <div class="admin show">
+					<h1>User: <?php echo h($user->full_name()); ?></h1>
+				
+					<div class="attributes">
 				    <dl>
 				     	<dt>First name</dt>
 				     	<dd><?php echo h($user->get_user_first_name()); ?></dd>
@@ -43,12 +41,17 @@ if(!$user = User::find_by_id($id)){
 				     	<dt>Created At</dt>
 				    	<dd><?php echo h($user->get_user_created_at()); ?></dd>
 				    </dl>
-              <a href="<?php echo url_for('/staff/users/content/index.php'); ?>">Bicycles</a>
 				</div>
 			</div>
-			<a href="./user_edit.php">Edit</a>
-			<br>
-			<a href="<?php echo url_for('./../index.php'); ?>">Home</a>
+			
+			<div id="outer">
+				<a class="inner" href="<?php echo url_for('/staff/users/content/index.php'); ?>">Bicycles</a>
+				<br />
+				<a class="inner" href="./user_edit.php">Edit</a>
+				<br />
+				<a class="inner" href="<?php echo url_for('./../index.php'); ?>">Home</a>
+			</div>
+
 		</div>
 	</div>
 </div>
