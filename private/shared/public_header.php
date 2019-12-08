1c01bc072f123a1e45cf4ba0c42f0edf47a773d3
Ioan Zîcu <ioan.zicu94@gmail.com>
2019-12-08 23:52:05 +0100

    add bootstrap, update navbar

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -3,10 +3,12 @@
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Iro's Bicycle Warehouse <?php if (isset($page_title)) {
                                     echo '- ' . h($page_title);
                                   } ?></title>
-    <meta charset="utf-8">
     <link rel="apple-touch-icon" sizes="57x57" href="<?php echo url_for(
                                                       '/favicon/apple-icon-60x60.png'
                                                     ); ?>">
@@ -43,26 +45,66 @@
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo url_for(
                                                           '/favicon/favicon-16x16.png'
                                                         ); ?>">
-    <link rel="manifest" href="/manifest.json">
+    <link rel="manifest" href="/favicon/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="<?php echo url_for(
                                                   '/favicon/ms-icon-144x144.png'
                                                 ); ?> ">
     <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" media="all" href="<?php echo url_for(
                                               '/css/public.css'
                                             ); ?>" />
+
 </head>
 
 <body>
-    <header>
-        <h1>
-            <a href="<?php echo url_for('/index.php'); ?>">
-                <img class="bike-icon" src="<?php echo url_for(
-                                      '/images/bicycle-icon.png'
-                                    ); ?>" />
-                <br />
-                Iro's Bicycle Warehouse
-            </a>
-        </h1>
-    </header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand font-weight-bold" href="<?php echo url_for('/index.php'); ?>"><img height="40"
+                src="<?php echo url_for(
+                                                                                                                  '/images/bicycle-icon.png'
+                                                                                                                ); ?>" />
+            Iro's Bicycle Warehouse</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse text-dark" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item ml-5">
+                    <a class="nav-link" href="<?php echo url_for(
+                                      '/content.php'
+                                    ); ?>">View Our Inventory </a>
+                </li>
+                <li class="nav-item ml-5">
+                    <a class="nav-link" href="<?php echo url_for('/about.php'); ?>">About Us</a>
+                </li>
+                <!-- ADMIN MENU BUTTON -->
+                <?php if (isset($_SESSION['username'])) : ?>
+                <li class="nav-item ml-5">
+                    <a class="nav-link" href="<?php echo url_for('/staff/index.php'); ?>">Admin Menu</a>
+                </li>
+                <li class="nav-item ml-5">
+                    <a class="nav-link" href="<?php echo url_for('/staff/logout.php'); ?>">Log Out</a>
+                </li>
+                <!-- USER ACCOUNT BUTTON -->
+                <?php elseif (isset($_COOKIE['username'])) : ?>
+                <li class="nav-item ml-5">
+                    <a class="nav-link" href="<?php echo url_for(
+                                          '/staff/users/user_account.php'
+                                        ); ?>">User Account</a>
+                </li>
+                <li class="nav-item ml-5">
+                    <a class="nav-link" href="<?php echo url_for('/staff/logout.php'); ?>">Log Out</a>
+                </li>
+                <!-- LOG IN BUTTON -->
+                <?php else : ?>
+                <li class="nav-item ml-5">
+                    <a class="nav-link" href="<?php echo url_for('/staff/login.php'); ?>">Log In</a>
+                </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </nav>
--- public/index.php
+++ public/index.php
@@ -2,31 +2,6 @@
 
 <?php include SHARED_PATH . '/public_header.php'; ?>
 
-<div id="main">
-
-    <ul id="menu">
-        <li><a href="<?php echo url_for(
-                    '/content.php'
-                  ); ?>">View Our Inventory</a></li>
-        <li><a href="<?php echo url_for('/about.php'); ?>">About Us</a></li>
-        <!-- ADMIN MENU BUTTON -->
-        <?php if (isset($_SESSION['username'])) : ?>
-        <li><a href="<?php echo url_for('/staff/index.php'); ?>">Admin Menu</a></li>
-        <li><a href="<?php echo url_for('/staff/logout.php'); ?>">Log Out</a></li>
-        <!-- USER ACCOUNT BUTTON -->
-        <?php elseif (isset($_COOKIE['username'])) : ?>
-        <li><a href="<?php echo url_for(
-                        '/staff/users/user_account.php'
-                      ); ?>">User Account</a></li>
-        <li><a href="<?php echo url_for('/staff/logout.php'); ?>">Log Out</a></li>
-        <!-- LOG IN BUTTON -->
-        <?php else : ?>
-        <li><a href="<?php echo url_for('/staff/login.php'); ?>">Log In</a></li>
-        <?php endif; ?>
-
-    </ul>
-</div>
-
 <?php $super_hero_image = 'main.jpg'; ?>
 
 <?php include SHARED_PATH . '/public_footer.php'; ?>
