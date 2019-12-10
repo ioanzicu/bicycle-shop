bb1da2c580fbe918781c5f798196ac7991e77705
Ioan Zîcu <ioan.zicu94@gmail.com>
2019-12-10 18:16:18 +0100

    add bootstrap to the about page

--- private/shared/public_copyright_disclaimer.php
+++ private/shared/public_copyright_disclaimer.php
@@ -1,7 +1,6 @@
-<div class="card-header">
-    <p class="pt-2 text-center">Copyright <?php echo date('Y'); ?>, Iro's Bicycle Warehouse.</p>
-</div>
 <div class="card-body text-center">
+    <p class="pt-1 text-center">&copy; Copyright <?php echo date('Y'); ?>, Iro's Bicycle Warehouse.</p>
+    <hr style="background-color:white;" />
     <p class="pl-5 pr-5">This is a fictitious company created by <a href="https://github.com/ioanzicu">Ioan
             Zicu</a>,
         solely for the development of the programming skill. Any resemblance to real products or
