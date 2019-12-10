3a2f8b01ce0569f03ff3743c6bded8a3cc7d6fd2
Ioan Zîcu <ioan.zicu94@gmail.com>
2019-12-10 19:42:30 +0100

    add bootstrap to the content page

--- private/shared/public_copyright_disclaimer.php
+++ private/shared/public_copyright_disclaimer.php
@@ -2,7 +2,7 @@
     <p class="pt-1 text-center">&copy; Copyright <?php echo date('Y'); ?>, Iro's Bicycle Warehouse.</p>
     <hr style="background-color:white;" />
     <p class="pl-5 pr-5">This is a fictitious company created by <a href="https://github.com/ioanzicu">Ioan
-            Zicu</a>,
+            Zîcu</a>,
         solely for the development of the programming skill. Any resemblance to real products or
         services is purely coincidental. Information provided about the products or services is also
         fictitious and should not be construed as representative of actual products or services on the
--- public/content.php
+++ public/content.php
@@ -3,92 +3,101 @@
 <?php $page_title = 'Inventory'; ?>
 <?php include SHARED_PATH . '/public_header.php'; ?>
 
-<div id="main">
 
-    <div id="page">
-        <div class="intro">
-            <img class="inset" src="<?php echo url_for('/images/gear.jpg'); ?>" />
-            <h2>Our Inventory of Used Bicycles</h2>
-            <p>Choose the bike you love.</p>
-            <p>We will deliver it to your door and let you try it before you buy it.</p>
-        </div>
+<div class="container">
+    <div class="text-center pt-3">
+        <h1>Our Inventory of Used Bicycles</h1>
+        <p>Choose the bike you love.</p>
+        <img class="inset" src="<?php echo url_for('/images/gear.jpg'); ?>" />
+        <p>We will deliver it to your door and let you try it before you buy it.</p>
+    </div>
 
-        <?php
-        $sort = ""; // Check if the submited variable exist in the $_POST array and it's value
-        if (isset($_POST["default"]) && $_POST["default"] === "default") {
-            $sort = "";
-        } elseif (isset($_POST["brand"]) && $_POST["brand"] === "brand") {
-            $sort = "brand";
-        } elseif (isset($_POST["model"]) && $_POST["model"] === "model") {
-            $sort = "model";
-        } elseif (isset($_POST["year"]) && $_POST["year"] === "year") {
-            $sort = "year";
+    <?php
+    $sort = ""; // Check if the submited variable exist in the $_POST array and it's value
+    if (isset($_POST["default"]) && $_POST["default"] === "default") {
+        $sort = "";
+    } elseif (isset($_POST["brand"]) && $_POST["brand"] === "brand") {
+        $sort = "brand";
+    } elseif (isset($_POST["model"]) && $_POST["model"] === "model") {
+        $sort = "model";
+    } elseif (isset($_POST["year"]) && $_POST["year"] === "year") {
+        $sort = "year";
+    }
+    //  ['Road', 'Mountain', 'Hybrid', 'Cruiser', 'City', 'BMX']
+    elseif (
+        isset($_POST["category"]) && ($_POST["category"] === "Road" ||
+            $_POST["category"] === "Mountain" ||
+            $_POST["category"] === "Hybrid" ||
+            $_POST["category"] === "Cruiser" ||
+            $_POST["category"] === "City" ||
+            $_POST["category"] === "BMX")
+    ) {
+        // echo $_POST["category"];
+        $sort = "category";
+        $category = "";
+        // Check cases for Select Menu
+        if ($_POST["category"] === "Road") {
+            $category = "Road";
+        } elseif ($_POST["category"] === "Mountain") {
+            $category = "Mountain";
+        } elseif ($_POST["category"] === "Hybrid") {
+            $category = "Hybrid";
+        } elseif ($_POST["category"] === "Cruiser") {
+            $category = "Cruiser";
+        } elseif ($_POST["category"] === "City") {
+            $category = "City";
+        } elseif ($_POST["category"] === "BMX") {
+            $category = "BMX";
         }
-        //  ['Road', 'Mountain', 'Hybrid', 'Cruiser', 'City', 'BMX']
-        elseif (
-            isset($_POST["category"]) && ($_POST["category"] === "Road" ||
-                $_POST["category"] === "Mountain" ||
-                $_POST["category"] === "Hybrid" ||
-                $_POST["category"] === "Cruiser" ||
-                $_POST["category"] === "City" ||
-                $_POST["category"] === "BMX")
-        ) {
-            // echo $_POST["category"];
-            $sort = "category";
-            $category = "";
-            // Check cases for Select Menu
-            if ($_POST["category"] === "Road") {
-                $category = "Road";
-            } elseif ($_POST["category"] === "Mountain") {
-                $category = "Mountain";
-            } elseif ($_POST["category"] === "Hybrid") {
-                $category = "Hybrid";
-            } elseif ($_POST["category"] === "Cruiser") {
-                $category = "Cruiser";
-            } elseif ($_POST["category"] === "City") {
-                $category = "City";
-            } elseif ($_POST["category"] === "BMX") {
-                $category = "BMX";
-            }
+    }
+    //  ['Mens', 'Womens', 'Unisex']
+    elseif (
+        isset($_POST["gender"]) && ($_POST["gender"] === "Mens" ||
+            $_POST["gender"] === "Womens" ||
+            $_POST["gender"] === "Unisex")
+    ) {
+        $sort = "gender";
+        $gender = "";
+        // Check cases for Select Menu
+        if ($_POST["gender"] === "Mens") {
+            $gender = "Mens";
+        } elseif ($_POST["gender"] === "Womens") {
+            $gender = "Womens";
+        } elseif ($_POST["gender"] === "Unisex") {
+            $gender = "Unisex";
         }
-        //  ['Mens', 'Womens', 'Unisex']
-        elseif (
-            isset($_POST["gender"]) && ($_POST["gender"] === "Mens" ||
-                $_POST["gender"] === "Womens" ||
-                $_POST["gender"] === "Unisex")
-        ) {
-            $sort = "gender";
-            $gender = "";
-            // Check cases for Select Menu
-            if ($_POST["gender"] === "Mens") {
-                $gender = "Mens";
-            } elseif ($_POST["gender"] === "Womens") {
-                $gender = "Womens";
-            } elseif ($_POST["gender"] === "Unisex") {
-                $gender = "Unisex";
-            }
-        } elseif (isset($_POST["color"]) && $_POST["color"] === "color") {
-            $sort = "color";
-        } elseif (isset($_POST["price"]) && $_POST["price"] === "price") {
-            $sort = "price";
-        } else {
-            $sort = "";
-        }
-        ?>
+    } elseif (isset($_POST["color"]) && $_POST["color"] === "color") {
+        $sort = "color";
+    } elseif (isset($_POST["price"]) && $_POST["price"] === "price") {
+        $sort = "price";
+    } else {
+        $sort = "";
+    }
+    ?>
 
-        <table id="inventory">
+    <table class="table table-bordered table-hover">
+        <thead class="thead-dark">
             <tr>
+                <th scope="col">#</th>
                 <form method="post" action="content.php">
                     <!-- Default State -->
-                    <input class="sort-button" type="submit" name="default" value="default" />
-
-                    <th><input class="sort-button" type="submit" name="brand" value="brand" /></th>
-                    <th><input class="sort-button" type="submit" name="model" value="model" /></th>
-                    <th><input class="sort-button" type="submit" name="year" value="year" /></th>
-
-                    <th>
+                    <!-- <input class="btn btn-dark uppercase" type="submit" name="default" value="default" /> -->
+                    <!-- Brand Filter -->
+                    <th scope="col">
+                        <input class="btn btn-dark uppercase" type="submit" name="brand" value="brand" />
+                    </th>
+                    <!-- Model Filter -->
+                    <th scope="col">
+                        <input class="btn btn-dark uppercase" type="submit" name="model" value="model" />
+                    </th>
+                    <!-- Year Filter -->
+                    <th scope="col">
+                        <input class="btn btn-dark uppercase" type="submit" name="year" value="year" />
+                    </th>
+                    <!-- Category Filter -->
+                    <th scope="col">
                         <!-- ['Road', 'Mountain', 'Hybrid', 'Cruiser', 'City', 'BMX'] -->
-                        <select class="sort-button" name="category" onchange="this.form.submit()">
+                        <select class="btn btn-dark uppercase" name="category" onchange="this.form.submit()">
                             <option selected>Category</option>
                             <option value="Road">Road</option>
                             <option value="Mountain">Mountain</option>
@@ -100,9 +109,10 @@
                         <noscript><input type="submit" value="submit"></noscript>
                     </th>
 
-                    <th>
+                    <!-- Gender Filter -->
+                    <th scope="col">
                         <!-- ['Mens', 'Womens', 'Unisex'] -->
-                        <select class="sort-button" name="gender" onchange="this.form.submit()">
+                        <select class="btn btn-dark uppercase" name="gender" onchange="this.form.submit()">
                             <option selected>Gender</option>
                             <option value="Mens">Mens</option>
                             <option value="Womens">Womens</option>
@@ -110,11 +120,16 @@
                         </select>
                         <noscript><input type="submit" value="submit"></noscript>
                     </th>
-                    <th><input class="sort-button" type="submit" name="color" value="color" /></th>
-                    <th><input class="sort-button" type="submit" name="price" value="price" /></th>
-                    <th>&nbsp;</th>
-
+                    <!-- Color Filter -->
+                    <th scope="col"><input class="btn btn-dark uppercase" type="submit" name="color" value="color" />
+                    </th>
+                    <!-- Price Filter -->
+                    <th scope="col"><input class="btn btn-dark uppercase" type="submit" name="price" value="price" />
+                    </th>
+                    <th scope="col">&nbsp;</th>
             </tr>
+        </thead>
+        <tbody>
             </form>
             <?php // Sort the data in the database and receive it
 
@@ -150,18 +165,20 @@
             <?php if (sizeof($bikes) === 0) : ?>
             <h1> There are not records with such criteria </h1>
             <!-- Fill the table with the data -->
-            <?php else : ?>
+            <?php else :
+                    $number_of_order = 0; ?>
 
             <?php foreach ($bikes as $bike) { ?>
-            <tr>
+            <tr scope="row">
+                <td><?php echo ++$number_of_order . '.'; ?></td>
                 <td><?php echo h($bike->get_content_brand()); ?></td>
                 <td><?php echo h($bike->get_content_model()); ?></td>
                 <td><?php echo h($bike->get_content_year()); ?></td>
                 <td><?php echo h($bike->get_content_category()); ?></td>
                 <td><?php echo h($bike->get_content_gender()); ?></td>
-                <td class="transform-color-case"><?php echo h(
-                                                                            $bike->get_content_color()
-                                                                        ); ?></td>
+                <td class="uppercase"><?php echo h(
+                                                                $bike->get_content_color()
+                                                            ); ?></td>
                 <td><?php echo h(
                                             money_format('$%i', $bike->get_content_price())
                                         ); ?></td>
@@ -169,9 +186,8 @@
             </tr>
             <?php } ?>
             <?php endif; ?>
-        </table>
-
-    </div>
+        </tbody>
+    </table>
 
 </div>
 
